2fdda9693cdcb7704f39b9cc50c6d1ff48908920
JanVerhoeven <zoete.inval.bevel@gmail.com>
2019-11-26 20:55:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleAppReadCSVFile
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                const string Path = @"C:\Users\zoete\Desktop\Prog4\ConsoleAppReadCSVFile\Prijzen_Jun19(beperkt).csv";
+                string strCSV = File.ReadAllText(Path);
+                Console.WriteLine(strCSV);
+                Console.Read();
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(Ex.Message);
+            }
+        }
+    }
+}
